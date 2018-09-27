216050a1bae6aad9c36611a4b5386664c9f6c401
Sudhanshu Gupta <sudhanshuom1996@gmail.com>
2018-09-27 21:52:32 +0530

    updated on 27-09-2018 9:37 PM

--- app/src/main/java/com/demo/mytask/MainActivity.java
+++ app/src/main/java/com/demo/mytask/MainActivity.java
@@ -14,12 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DishDetail bill_detail = new DishDetail("Biryani Fafda","Half Plate","67 $","1 h","true","gs://notification-fe2b9.appspot.com/dishImage/Biryani/501141009-612x612.jpg");
-           FirebaseDatabase database = FirebaseDatabase.getInstance();
-          DatabaseReference databaseReference = database.getReference().child("Indian").child("Kolkata");
-        databaseReference.push().setValue(bill_detail);
+//        DishDetail bill_detail = new DishDetail("Biryani Fafda","Half Plate","67 $","1 h","true","gs://notification-fe2b9.appspot.com/dishImage/Biryani/501141009-612x612.jpg");
+//           FirebaseDatabase database = FirebaseDatabase.getInstance();
+//          DatabaseReference databaseReference = database.getReference().child("Indian").child("Kolkata");
+//        databaseReference.push().setValue(bill_detail);
 
-        Intent intent=new Intent(this,ListOoFDishes.class);
+        Intent intent=new Intent(this,AddItem.class);
         startActivity(intent);
 
 
